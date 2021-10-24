db3451f52da67c940a7b4161248e48fb9c634ee4
theoptips <optanovo@gmail.com>
2021-10-24 14:40:22 -0700

    Create code_aws_sagemaker_xgboost.py

---
+++ code_aws_sagemaker_xgboost.py
@@ -0,0 +1,38 @@
+import boto3
+import sagemaker
+from sagemaker.xgboost.estimator import XGBoost
+from sagemaker.session import s3_input, Session
+
+# initialize hyperparameters
+hyperparameters = {
+        "max_depth":"5",
+        "eta":"0.2",
+        "gamma":"4",
+        "min_child_weight":"6",
+        "subsample":"0.7",
+        "verbosity":"1",
+        "objective":"reg:linear",
+        "num_round":"50"}
+
+# set an output path where the trained model will be saved
+bucket = sagemaker.Session().default_bucket()
+prefix = 'DEMO-xgboost-as-a-framework'
+output_path = 's3://{}/{}/{}/output'.format(bucket, prefix, 'abalone-xgb-framework')
+
+# construct a SageMaker XGBoost estimator
+# specify the entry_point to your xgboost training script
+estimator = XGBoost(entry_point = "your_xgboost_abalone_script.py", 
+                    framework_version='1.2-2',
+                    hyperparameters=hyperparameters,
+                    role=sagemaker.get_execution_role(),
+                    instance_count=1,
+                    instance_type='ml.m5.2xlarge',
+                    output_path=output_path)
+
+# define the data type and paths to the training and validation datasets
+content_type = "libsvm"
+train_input = s3_input("s3://{}/{}/{}/".format(bucket, prefix, 'train'), content_type=content_type)
+validation_input = s3_input("s3://{}/{}/{}/".format(bucket, prefix, 'validation'), content_type=content_type)
+
+# execute the XGBoost training job
+estimator.fit({'train': train_input, 'validation': validation_input})
