2d70a1d76d33738de2e66b2d12a7e91019256b1a
theoptips <optanovo@gmail.com>
2021-11-02 02:03:01 -0700

    Create pytorch_first_tensor.py

---
+++ pytorch_first_tensor.py
@@ -0,0 +1,17 @@
+'''
+
+
+	# create your first random tensor of shape 3x4
+	# try on Google Colab
+	# no installation needed
+	# https://colab.research.google.com/
+
+
+
+'''
+
+import torch
+
+x = torch.rand(3, 4)
+
+print(x)
